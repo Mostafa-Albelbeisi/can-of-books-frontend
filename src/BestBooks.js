32efe7cc99bfd8962149c7d1d6f5c5577fb4e237
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2022-09-04 19:31:18 +0300

    v0.1

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,20 +10,44 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-
+  componentDidMount = () =>{
+    axios
+    .get(`http://localhost:3001/getBooks`)
+    .then(result => {
+      console.log(result.data);
+      this.setState({
+        books : result.data
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   render() {
 
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
+
+        <div> 
+        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        {this.state.books.map(item => {
+          <div>
+            <h3>Book title: {item.title}</h3>
+            <h3>Book description: {item.discreption}</h3>
+            <h3>Book status: {item.status}</h3>
+          </div>
+        })}
+        </div>
+
+        {/* {this.state.books.length ? (
           <p>Book Carousel coming soon</p>
         ) : (
           <h3>No Books Found :(</h3>
-        )}
+        )} */}
+        
       </>
     )
   }
